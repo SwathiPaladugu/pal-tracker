e4f9b683fbc0ad083a5eb196cd2024f65b33d286
Ubuntu <ubuntu@ip-172-31-83-13.ec2.internal>
2021-08-19 17:00:51 +0000

    clean up

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -2,7 +2,6 @@ package io.pivotal.pal.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
@@ -15,7 +14,7 @@ public class PalTrackerApplication {
     }
 
     @Bean
-        public TimeEntryRepository getTimeEntryRepository(DataSource datasource) {
-            return new JdbcTimeEntryRepository(datasource);
-        }
+    public TimeEntryRepository getTimeEntryRepository(DataSource datasource) {
+        return new JdbcTimeEntryRepository(datasource);
+    }
 }
